0a4d2eb94b350a0fa5950be7609518019733c7f6
AXinx <xin-yue-2009@163.com>
2022-01-17 20:20:27 +0100

    change rep

--- install-caliper.sh
+++ install-caliper.sh
@@ -2,8 +2,8 @@ cd ~
 sudo apt-get install make g++
 sudo npm install -g node-gyp
 
-git clone https://github.com/AXinx/caliper-bench.git
-cd ./caliper-bench/azure-3nodes/caliper-benchmarks/
+git clone https://github.com/AXinx/caliper-benchmarks.git
+cd ./caliper-benchmarks/
 npm install --save fs-extra 
 npm install -g --only=prod @hyperledger/caliper-cli@0.2.0 --unsafe-perm
 npx caliper bind --caliper-bind-sut fabric --caliper-bind-sdk 1.4.4 --caliper-bind-cwd ./
